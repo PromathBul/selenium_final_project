b2d64c8e3932841059366c3f44f7d4bb70d41609
Tikhonov Dmitrii <tdmv1209@gmail.com>
2025-03-13 15:14:11 +0300

    Добавлена фикстура для запуска и закрытия браузера Chrome, написан тест для проверки возможности нажать на ссылку для входа ил регистрации

--- conftest.py
+++ conftest.py
@@ -2,6 +2,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def pytest_addoption(request):
-    request.addoption('--language', action='store', default='en', help='Choose any language: ru, en, es, fr etc.')
-    
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Choose any language: ru, en, es, fr etc.')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    print('\nbrowser starts for test...')
+    yield browser
+    print('\nbrowser quit')
+    browser.quit()
