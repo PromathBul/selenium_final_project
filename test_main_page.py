2463fd10702ed4ac2aef7bc7bd4c9d0c323a8d1f
Tikhonov Dmitrii <tdmv1209@gmail.com>
2025-03-14 12:17:53 +0300

    Логика поиска и нажатия кнопки c логином вынесена в отдельную функцию

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
 
 LINK = 'http://selenium1py.pythonanywhere.com/'
-def test_guest_can_go_to_login_page(browser):
-    browser.get(LINK)
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
     login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    browser.get(LINK)
+    go_to_login_page(browser)
